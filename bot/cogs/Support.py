125edbfdf55c7ec0ca3f236ee68b622a92214f14
pusheen3 <theofficialjerrypy@gmail.com>
2022-01-07 18:16:44 -0500

    Remove Currency will rewrite in 1.1

--- bot/cogs/Support.py
+++ bot/cogs/Support.py
@@ -3,13 +3,6 @@ import discord
 from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 
-import os
-from bot.cogs.Currency import Currency
-
-
-
-
-
 class Support(commands.Cog):
     def __init__(self, client):
         self.client = client
